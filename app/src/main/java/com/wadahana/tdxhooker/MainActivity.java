97f0aaf3002b1ede3110b6da48a48c0cc2bb3916
eric-ng <wadahana@qq.com>
2022-05-04 21:01:45 +0800

    add updateButton and testButton

--- app/src/main/java/com/wadahana/tdxhooker/MainActivity.java
+++ app/src/main/java/com/wadahana/tdxhooker/MainActivity.java
@@ -1,22 +1,93 @@
 package com.wadahana.tdxhooker;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.wadahana.tdxhooker.common.HookHelper;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 public class MainActivity extends AppCompatActivity {
     static String kTag = "TdxHooker";
+    private Button updateButton;
+    private Button testButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        updateButton = (Button)findViewById(R.id.UpdateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                copyLibrarys();
+            }
+        });
+        testButton = (Button)findViewById(R.id.TestButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
         Log.i(kTag, "MainActivity onCreate");
         //Log.i(kTag, "IMEI:" + getIMEI(this));
+    }
+    public void copyLibrarys() {
+        String appPath = getApplicationContext().getPackageResourcePath();
+        int i = appPath.indexOf("/base.apk");
+        if (i == -1) {
+            Log.e(kTag, "appPath: " + appPath + " malformed!");
+            return ;
+        }
+        String basePath = appPath.substring(0, i);
+        String libPath = basePath + "/lib/arm64";
+        String dstPath = Environment.getRootDirectory().getPath() ;
+        Log.i(kTag, "MainActivity app path:" + libPath);
+        Log.i(kTag, "MainActivity dst path:" + dstPath);
+        //copy(libPath + "/libtdx-hooker.so", dstPath + "/lib64/libtdx-hooker.so");
+        String cmd = "cp " + libPath + "/libtdx-hooker.so" + " " + dstPath + "/lib64/libtdx-hooker.so" + "\n";
+        Log.i(kTag, "MainActivity cmd: " + cmd);
+        try {
+            Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream dos = new DataOutputStream(p.getOutputStream());
+            dos.writeBytes(cmd);
+            dos.flush();
+        } catch (IOException e) {
+            Log.e(kTag, "MainActivity copy libtdxhooker.so fail: " + e);
+        }
+        return ;
+    }
+    public void copy(String src, String dst) {
+        try {
+            File inFile = new File(src);
+            File outFile = new File(dst);
+            InputStream inputStream = new FileInputStream(inFile);
+            OutputStream outputStream = new FileOutputStream(outFile);
+            byte[] buffer= new byte[1024];
+            while(true) {
+                int len = inputStream.read(buffer);
+                if (len == -1) {
+                    break;
+                }
+                outputStream.write(buffer,0, len);
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e(kTag, "MainActivity copy file fail: " + e);
+        }
+        return;
     }
     public static final String getIMEI(Context context) {
         HookHelper.dumpCallStack();

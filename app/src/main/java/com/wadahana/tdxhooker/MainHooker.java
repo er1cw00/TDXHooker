85b46273a934c251b6147ec21e2803ebbb64e46a
eric-ng <wadahana@qq.com>
2022-05-04 20:59:31 +0800

    rename MainHooker

--- app/src/main/java/com/wadahana/tdxhooker/MainHooker.java
+++ app/src/main/java/com/wadahana/tdxhooker/MainHooker.java
@@ -4,15 +4,15 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class HookLoader implements IXposedHookLoadPackage {
-    static String kTag = "HookLoader";
+public class MainHooker implements IXposedHookLoadPackage {
+    static String kTag = "MainHooker";
     final String packgeName = "com.tdx.AndroidNew";
+    private NativeHooker nativeHooker = new NativeHooker();
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals(packgeName)) {
-            XposedBridge.log("not hook package:" + lpparam.packageName);
+            XposedBridge.log("TDXHooker not hook package:" + lpparam.packageName);
             return;
         }
-        XposedBridge.log("hook package:" + lpparam.packageName);
     }
 }
